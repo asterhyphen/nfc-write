ae2f04609ab054516c64ed3fb8a8a65cb05cd54a
Ahmed Siddiqua <ahmedsiddiqua414@gmail.com>
2026-08-28 15:08:55 +0530

    Update compileSdk and targetSdk version to 36 to resolve compile errors in modern plugins

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -9,7 +9,6 @@ val newBuildDir: Directory =
     rootProject.layout.buildDirectory
         .dir("../../build")
         .get()
-// Set the build directory at the root project level.
 rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
@@ -22,11 +21,19 @@ subprojects {
 }
 
 subprojects {
-    afterEvaluate {
-        if (plugins.hasPlugin("com.android.application") || plugins.hasPlugin("com.android.library")) {
-            configure<com.android.build.gradle.BaseExtension> {
-                compileSdkVersion(34)
+    val configureProject = { proj: Project ->
+        if (proj.plugins.hasPlugin("com.android.application") || proj.plugins.hasPlugin("com.android.library")) {
+            proj.configure<com.android.build.gradle.BaseExtension> {
+                compileSdkVersion(36)
             }
+        }
+    }
+
+    if (project.state.executed) {
+        configureProject(project)
+    } else {
+        project.afterEvaluate {
+            configureProject(project)
         }
     }
 }
