bf444938ea11fa25120315368e2e1d95fe1a23e1
Ahmed Siddiqua <ahmedsiddiqua414@gmail.com>
2026-08-28 15:07:30 +0530

    Configure compileSdk and targetSdk to 34 and force plugin subprojects to compile against SDK 34 to resolve build errors

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -9,14 +9,26 @@ val newBuildDir: Directory =
     rootProject.layout.buildDirectory
         .dir("../../build")
         .get()
+// Set the build directory at the root project level.
 rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
+}
+
+subprojects {
+    afterEvaluate {
+        if (plugins.hasPlugin("com.android.application") || plugins.hasPlugin("com.android.library")) {
+            configure<com.android.build.gradle.BaseExtension> {
+                compileSdkVersion(34)
+            }
+        }
+    }
 }
 
 tasks.register<Delete>("clean") {
